cdad304e3df033bcb02ba41a637b432cec8497a4
Leonardo Ferreira <56656888+leonardo-ferreiraa@users.noreply.github.com>
2024-02-02 14:13:49 -0300

    adicionando os arquivos ao repositório

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,9 @@
+import "./Banner.css";
+
+const Banner = (props) => {
+    return (<header className="banner">
+    <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+</header>)
+}
+
+export default Banner
